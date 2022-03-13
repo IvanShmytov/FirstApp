d3b42604ff844fbbf4bb44c0ca4c53e9dd2ae29b
IvanShmytov <ivanshmytov@mail.ru>
2022-03-13 16:01:29 +0300

    Выполнены задания 4.3.12, 4.3.13 и 4.3.14.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,14 +4,44 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
-        for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+        Console.WriteLine("Сортировка массива { 5, 6, 9, 1, 2, 3, 4}:");
+        var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+        int temp;
+        for (int i = 0; i < arr.Length; i++)
         {
-            for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+            for (int j = i + 1; j < arr.Length; j++)
             {
-                Console.Write(array[k,i] + " ");
+                if (arr[i] > arr[j])
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
             }
-
+        }
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.Write("\nСумма элементов массива { 5, 6, 9, 1, 2, 3, 4 }: ");
+        var arr2 = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+        int sum = 0;
+        for (int i = 0; i < arr2.Length; i++)
+        {
+            sum += arr2[i];
+        }
+        Console.WriteLine(sum);
+        Console.WriteLine("Отображение зубчатого массива:");
+        int[][] arr3 = new int[3][];
+        arr3[0] = new int[2] { 1, 2 };
+        arr3[1] = new int[3] { 1, 2, 3 };
+        arr3[2] = new int[5] { 1, 2, 3, 4, 5 };
+        foreach (var item in arr3) 
+        {
+            foreach (var item2 in item) 
+            {
+                Console.Write(item2 + " ");
+            }
             Console.WriteLine();
         }
     }
