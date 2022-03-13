36f8bc631921640379b554c4ad23dbbd0754107c
IvanShmytov <ivanshmytov@mail.ru>
2022-03-13 16:40:40 +0300

    Выполнено задание 4.3.16.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,20 +4,23 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        int[] arr = { 1, 100, 0, -1, 5, -100, 14 };
+        int[][] arr = new int[2][];
+        arr[0] = new int []{ -5, 6, 9, 1, 2, -3 };
+        arr[1] = new int[] { -8, 8, 1, 1, 2, -3 };
         int PosNum = 0;
-        foreach (var item in arr) 
+        Console.WriteLine("Количество положительных чисел в двухмерном массиве: ");
+        foreach (var item in arr)
         {
-            if (item > 0) 
+            foreach (var item2 in item)
             {
-                PosNum++;
+                Console.Write(item2 + " ");
+                if (item2 > 0) 
+                {
+                    PosNum++;
+                }
             }
+            Console.WriteLine();
         }
-        Console.Write("Количество положительных чисел в массиве {");
-        foreach (var item in arr) 
-        {
-            Console.Write(item + " ");
-        }
-        Console.Write("} - " + PosNum);
+        Console.Write("= " + PosNum);
     }
 }
