ea7fa60a103104e0ff8c8ad01c54f3914e927606
IvanShmytov <ivanshmytov@mail.ru>
2022-03-13 14:19:22 +0300

    Выполнено задание 4.3.4.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,36 +4,13 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        int t = 0;
-        do {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-            var color = Console.ReadLine();
-            Console.WriteLine("Iteration {0}", t);
-            if (color == "stop") { break; }
-            switch (color)
-            {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your color is red");
-                    break;
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your color is green");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your color is cyan");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("The color is unknown");
-                    break;
-            }
-            t++;
-        } while (t < 3);
+        Console.WriteLine("Введите свое имя");
+        string name = Console.ReadLine();
+        Console.WriteLine("Ваше имя по буквам:");
+        foreach (var item in name) 
+        {
+            Console.Write(item + " ");
         }
+        Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
+    }
 }
