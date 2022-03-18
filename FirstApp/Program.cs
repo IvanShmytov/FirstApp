cf3a7844ea12f87ca80d6409e35baf62e3fb4014
IvanShmytov <ivanshmytov@mail.ru>
2022-03-18 20:28:28 +0300

    Выполнено задание 4.5.6.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,14 +4,34 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        (string Name, string Type, double Age, int NameCount) Pet;
-        Console.WriteLine("Введите имя питомца");
-        Pet.Name = Console.ReadLine();
-        Console.WriteLine("Введите тип питомца");
-        Pet.Type = Console.ReadLine();
-        Console.WriteLine("Введите возраст питомца");
-        Pet.Age = Convert.ToInt32(Console.ReadLine());
-        Pet.NameCount = Pet.Name.Length;
-        Console.WriteLine("Имя питомца: {0}\nТип питомца: {1}\nВозраст питомца: {2}\nДлинна имени питомца {3}", Pet.Name, Pet.Type, Pet.Age, Pet.NameCount);
+        (string Name, string Surname, string Login, int LoginLength, bool HavePet, string[] FavColors, int Age) User;
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию");
+            User.Surname = Console.ReadLine();
+            Console.WriteLine("Введите логин");
+            User.Login = Console.ReadLine();
+            User.LoginLength = User.Login.Length;
+            Console.WriteLine("Введите возраст пользователя");
+            User.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Есть ли у вас животные? Да или Нет");
+            string answer = Console.ReadLine();
+            if (answer == "Да")
+            {
+                User.HavePet = true;
+            }
+            else 
+            {
+                User.HavePet = false;
+            }
+            Console.WriteLine("Введите три любимых цвета пользователя");
+            User.FavColors = new string[3];
+            for (int j = 0; j < User.FavColors.Length; j++)
+            {
+                User.FavColors[j] = Console.ReadLine();
+            }
+        }
     }
 }
