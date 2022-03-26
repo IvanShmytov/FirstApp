fb46ff8ad6a73ec34a06119e8044c9fa5d4da877
IvanShmytov <ivanshmytov@mail.ru>
2022-03-26 11:37:53 +0300

    Выполнено задание 5.5.8.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,25 +4,25 @@ class MainClass
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Напишите что-то");
-        var str = Console.ReadLine();
-        Console.WriteLine("Укажите глубину эха");
-        var deep = int.Parse(Console.ReadLine());
-        Echo(str, deep);
+        Console.WriteLine(PowerUp(2,5));
+
     }
-    static void Echo(string phrase, int deep)
+
+    private static int PowerUp(int num, byte pow) 
     {
-        var modif = phrase;
-        if (modif.Length > 2)
+        if (pow == 0)
         {
-            modif = modif.Remove(0, 2);
+            return 1;
         }
-        Console.BackgroundColor = (ConsoleColor)deep;
-        Console.WriteLine("..." + modif);       
-        if (deep > 1)
+        else if (pow == 1)
         {
-            Echo(modif, deep - 1);
+            return num;
         }
+        else
+        {
+            return num * PowerUp(num, --pow);
+        }
+    
     }
 }
 
