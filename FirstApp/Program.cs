c48f9b10b546898db69239d85768fdc7a8655539
IvanShmytov <ivanshmytov@mail.ru>
2022-04-03 22:35:33 +0300

    Выполнено задание 7.2.4.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,24 +4,48 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-;
+        
     }
-
+   
     class BaseClass
     {
         protected string Name;
+        public virtual int Counter
+        {
+            get;
+            set;
+        }
 
         public BaseClass(string name)
         {
             Name = name;
+        }
+        public virtual void Display() 
+        {
+            Console.WriteLine("Метод класса BaseClass");
         }
     }
 
     class DerivedClass : BaseClass
     {
         public string Description;
-
-        public int Counter;
+        private int counter;
+        public override int Counter 
+        {
+            get { return counter; }
+            set 
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("число меньше 0");
+                }
+                else 
+                {
+                    counter = value;
+                }
+            }
+        
+        }
         public DerivedClass(string name, string descr, int count): this(name, descr) 
         {
             Counter = count;
@@ -30,6 +54,11 @@ class MainClass
         {
             Description = descr;
         }
+        public override void Display()
+        {
+            Console.WriteLine("Метод класса DerivedClass");
+        }
     }
+  
 }
 
