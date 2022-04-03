e8facd3707e4badce21fa0c1f890561d9dd656c7
IvanShmytov <ivanshmytov@mail.ru>
2022-04-03 17:37:15 +0300

    Выполнено задание 7.1.10.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,152 +1,35 @@
 ﻿using System;
 
-class Program
+class MainClass
 {
-	static void Main(string[] args)
-	{
-		User user = new User();
-		user.Age = 13;
-		Console.WriteLine(user.Age);
-		Console.ReadKey();
-	}
-	class Circle
-	{
-		public double radius;
+    public static void Main(string[] args)
+    {
+;
+    }
 
-		public double Square()
-		{
-			return 3.14 * Math.Pow(radius, 2);
-		}
+    class BaseClass
+    {
+        protected string Name;
 
-		public double Length()
-		{
-			return radius * 3.14 * 2;
-		}
-	}
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+    }
 
-	class Triangle
-	{
-		private int a;
-		public int b;
-		public int c;
+    class DerivedClass : BaseClass
+    {
+        public string Description;
 
-		public int A
-		{
-			get
-			{
-				return a;
-			}
-			set
-			{
-				if (value < 0 || value > b + c)
-				{
-					Console.WriteLine("Incorrect value");
-				}
-				else
-				{
-					a = value;
-				}
-
-			}
-		}
-		public int C
-		{
-			get
-			{
-				return c;
-			}
-			set
-			{
-				if (value < 0 || value > b + a)
-				{
-					Console.WriteLine("Incorrect value");
-				}
-				else
-				{
-					c = value;
-				}
-
-			}
-		}
-		public double Square()
-		{
-			return 2 * a * b;
-		}
-
-		public double Perimeter()
-		{
-			return a + b + c;
-		}
-	}
-
-	class Square
-	{
-		public int side;
-
-		public double SquareOfSquare()
-		{
-			return side * side;
-		}
-
-		public double Perimeter()
-		{
-			return side * 4;
-		}
-	}
-	class TrafficLight 
-	{
-		private string color;
-		private void ChangeColor(string color) { }
-		public string GetColor() 
-		{
-			return color;
-		}
-	}
-	class User
-	{
-		private int age;
-		private string login;
-		private string mail;
-		public int Age
-		{
-			get
-			{
-				return age;
-			}
-
-			set
-			{
-				if (value < 18)
-				{
-					Console.WriteLine("Возраст должен быть не меньше 18");
-				}
-				else
-				{
-					age = value;
-				}
-			}
-		}
-	
-		public string Login { get; private set; }
-		public string Mail
-		{
-			get
-			{
-				return mail;
-			}
-			set
-			{
-				if (!value.Contains('@'))
-				{
-					Console.WriteLine("адрес почты должен содержать символ '@'");
-				}
-				else
-				{
-					mail = value;
-				}
-			}
-		}
-	}
+        public int Counter;
+        public DerivedClass(string name, string descr, int count): this(name, descr) 
+        {
+            Counter = count;
+        }
+        public DerivedClass(string name, string descr) : base(name)
+        {
+            Description = descr;
+        }
+    }
 }
-
 
