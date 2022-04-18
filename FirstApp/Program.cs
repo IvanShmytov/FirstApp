29a4b821bbece98ee2b9b46bc4e98ea7ec059cb2
IvanShmytov <ivanshmytov@mail.ru>
2022-04-18 15:53:33 +0300

    Выполнены задания 8.3.1.-8.3.2.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,54 +4,26 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        //Counter();
-        Mover();
-    }
-    public static void Counter() 
-    {
-        try
+
+        string filePath = @"C:\Users\ivans\source\repos\FirstApp\FirstApp\Program.cs"; // Укажем путь 
+        FileInfo fileinfo = new FileInfo(filePath);
+
+        using (StreamWriter sw = fileinfo.AppendText()) 
         {
-            DirectoryInfo dirInfo = new DirectoryInfo(@"D:\\");
-            Console.WriteLine($"Название каталога: {dirInfo.Name}");
-            Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
-            Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
-            Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
-            if (dirInfo.Exists)
+            sw.WriteLine(DateTime.Now);
+        }
+        using (StreamReader sr = fileinfo.OpenText())
+        {
+            string str = "";
+            while ((str = sr.ReadLine()) != null)
             {
-                Console.WriteLine(dirInfo.GetDirectories().Length );
+                Console.WriteLine(str);
             }
-
-            DirectoryInfo newDirectory = new DirectoryInfo(@"D:\\NewDir2");
-            if (!newDirectory.Exists)
-                newDirectory.Create();
-
-            Console.WriteLine(dirInfo.GetDirectories().Length);
-            dirInfo = new DirectoryInfo(@"D:\\NewDir2");
-            dirInfo.Delete(true); // Удаление со всем содержимым
-            Console.WriteLine("Каталог удален");
-            dirInfo = new DirectoryInfo(@"D:\\");
-            Console.WriteLine(dirInfo.GetDirectories().Length);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
         }
     }
-    public static void Mover() 
-    {
-        try
-        {
-            DirectoryInfo dirInfo = new DirectoryInfo("C:\\Users\\ivans\\Desktop\\testFolder");
-            string trashPath = "C:\\Users\\ivans\\Desktop\\trash\\testFolder";
 
-            dirInfo.MoveTo(trashPath);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
-    }
-}
-  
    
+}
 
+
+//18.04.2022 15:52:58
