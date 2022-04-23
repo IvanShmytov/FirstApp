6905c280f7aa0301175483a4840786caff1f39b5
IvanShmytov <ivanshmytov@mail.ru>
2022-04-23 23:25:11 +0300

    Выполнены задания 9.1.3.-9.1.4.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,40 +4,9 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Contact con = new Contact("Victor", 333222, "victor@mail.ru");
-        BinaryFormatter form = new BinaryFormatter();
-        using (FileStream fs = new FileStream("Victor.dat", FileMode.OpenOrCreate)) 
-        {
-            form.Serialize(fs, con);
-            Console.WriteLine("Объект сериализован");
-        }
-        using (FileStream fs = new FileStream("Victor.dat", FileMode.OpenOrCreate)) 
-        {
-            Contact newCon = (Contact)form.Deserialize(fs);
-            Console.WriteLine("Объект десериализован");
-            Console.WriteLine(newCon); 
-        }
-    }
-    [Serializable]
-    class Contact
-    {
-        private string Name { get; set; }
-        private long PhoneNumber { get; set; }
-        private string Email { get; set; }
-
-        public Contact(string name, long phoneNumber, string email)
-        {
-            Name = name;
-            PhoneNumber = phoneNumber;
-            Email = email;
-        }
-        public override string ToString() 
-        {
-            string result = $"имя - {Name}\nтелефон - {PhoneNumber}\nемейл - {Email}";
-            return result; 
-        }
-    }
-
-   
+        Exception exc = new Exception("My exception");
+        exc.Data.Add("Дата создания исключения", DateTime.Now);
+        exc.HelpLink = "www.google.com";
+    }   
 }
 
