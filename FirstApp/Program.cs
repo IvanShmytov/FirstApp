548f2fb35e5d86daaba0456f62c152987fc1a672
IvanShmytov <ivanshmytov@mail.ru>
2022-04-24 17:13:39 +0300

    Выполнены задания 9.2.2.-9.2.3.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,6 +7,28 @@ class MainClass
         Exception exc = new Exception("My exception");
         exc.Data.Add("Дата создания исключения", DateTime.Now);
         exc.HelpLink = "www.google.com";
-    }   
+        try
+        {
+            Console.WriteLine("Блок Try начал работу");
+            throw new RankException();
+            throw new ArgumentOutOfRangeException();
+        }
+        catch (RankException ex) 
+        {
+            Console.WriteLine($"Тип исключения {ex.GetType()}");
+        }
+
+        catch (Exception ex) when (ex is ArgumentOutOfRangeException)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        finally
+        {
+            Console.WriteLine("Блок Finally сработал!");
+        }
+
+        Console.ReadKey();
+    }
 }
 
