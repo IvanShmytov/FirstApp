541bb2075611140fdfe52cac726efa5ea781ecbd
IvanShmytov <ivanshmytov@mail.ru>
2022-04-26 17:08:31 +0300

    Выполнены задания 9.3.14.-9.3.15.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,33 +2,17 @@
 using System.Runtime.Serialization.Formatters.Binary;
 class MainClass
 {
+    delegate void ShowMessageDelegate(string _message);
+    delegate int RandomNumberDelegate();
     public static void Main(string[] args)
     {
-        Exception exc = new Exception("My exception");
-        exc.Data.Add("Дата создания исключения", DateTime.Now);
-        exc.HelpLink = "www.google.com";
-        try
-        {
-            Console.WriteLine("Блок Try начал работу");
-            throw new RankException();
-            throw new ArgumentOutOfRangeException();
-        }
-        catch (RankException ex) 
-        {
-            Console.WriteLine($"Тип исключения {ex.GetType()}");
-        }
-
-        catch (Exception ex) when (ex is ArgumentOutOfRangeException)
-        {
-            Console.WriteLine(ex.Message);
-        }
-
-        finally
-        {
-            Console.WriteLine("Блок Finally сработал!");
-        }
-
-        Console.ReadKey();
+        ShowMessageDelegate showMessageDelegate = (string str) => Console.WriteLine(str);
+        showMessageDelegate.Invoke("Hello World!");
+        RandomNumberDelegate randomNumberDelegate = () => new Random().Next(0,100);
+        int result = randomNumberDelegate.Invoke();
+        Console.WriteLine(result);
+        Console.Read();
     }
+ 
 }
 
