800f251279db465c51dea989aab88c5376ab0f50
IvanShmytov <ivanshmytov@mail.ru>
2022-05-21 15:04:46 +0300

    Выполнены задания 13.2.5 - 13.2.6.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,16 +1,62 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
 namespace CountWords
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string Path = @"C:\Users\ivans\Desktop\cdev_Text.txt";
-            string Text = File.ReadAllText(Path);
-            char[] Dividers = new char[] { ' ', '\r', '\n', '\t' };
-            string[] NumbOfWords = Text.Split(Dividers, StringSplitOptions.RemoveEmptyEntries);
-            Console.WriteLine(NumbOfWords.Length);
+            var months = new[]
+            {
+               "Jan", "Feb", "Mar", "Apr", "May" , "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            };
+            var numbers = new[]
+            {
+               1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12
+            };
+            var ArrL = new ArrayList();
+            foreach (var number in numbers)
+            {
+                ArrL.Add(months[number - 1]);
+                ArrL.Add(number);
+            }
+            foreach (var item in ArrL)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            var NewArrl = new ArrayList()
+            {
+                1,
+                "Андрей ",
+                "Сергей ",
+                300,
+            };
+            var Result = ArrListConvert(NewArrl);
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static ArrayList ArrListConvert( ArrayList Input) 
+        {
+            int numbers = 0;
+            string text = "";
+            foreach (var item in Input)
+            {
+                if (item is int) 
+                {
+                    numbers += (int) item;
+                }
+                if (item is string)
+                {
+                    text += item;
+                }
+            }
+            ArrayList Output = new ArrayList() { numbers, text };
+            return Output;
         }
     }
 }
