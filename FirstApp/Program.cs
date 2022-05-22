e6635f986d53117362e98620e560831057aceb4f
IvanShmytov <ivanshmytov@mail.ru>
2022-05-22 17:10:27 +0300

    Выполнено задание 13.3.11.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,38 +9,28 @@ namespace CountWords
     {
         static void Main(string[] args)
         {
-            var months = new List<string>()
+            string text = "Подсчитайте, 1сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
+            var hSet = new HashSet<char>(text);
+            Console.WriteLine("Символов всего " + hSet.Count);
+            char[] symbols = new char[] {'.', ',', '!', '?', ' ' };
+            hSet.ExceptWith(symbols);
+            Console.WriteLine("Символов без знаков препинания " + hSet.Count);
+
+            //foreach (var item in hSet)
+            //{
+            //    if (char.IsDigit(item)) 
+            //    {
+            //        Console.WriteLine("В тексте есть цифры");
+            //        break;
+            //    }
+            //}
+
+            string numbers = null;
+            for (int i = 0; i < 10; i++)
             {
-                "Jan", "Feb", "Mar", "Apr", "May"
-            };
-            var missing = new ArrayList()
-            {
-                1, 2, 3, 5, "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-            };
-            //InsertMonths(missing, months);
-            GetMissing(months, missing);
-        }
-        static void InsertMonths(ArrayList mixed, List<string> result)
-        {
-            foreach (var item in mixed)
-            {
-                if (item is string)
-                {
-                    result.Add((string)item);
-                }
+                numbers += i;
             }
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
-        }
-        private static void GetMissing(List<string> months, ArrayList missing)
-        {
-            var missedArray = new string[7];
-            missing.GetRange(4, 7).CopyTo(missedArray);
-            months.AddRange(missedArray);
-            foreach (var month in months)
-                Console.WriteLine(month);
+            Console.WriteLine("В тексте есть цифры - " + hSet.Overlaps(numbers));
         }
     }
 }
