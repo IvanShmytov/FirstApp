c017dade90f0d9cc6c6b39f1b4ac97d2ecef4d7f
IvanShmytov <ivanshmytov@mail.ru>
2022-05-22 18:17:23 +0300

    Выполнено задание 13.4.4.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,29 +7,42 @@ namespace CountWords
 {
     class Program
     {
+        private static Dictionary<string, Contact> PhoneBook = new Dictionary<String, Contact>()
+        {
+            ["Игорь"] = new Contact(79990000000, "igor@example.com"),
+            ["Андрей"] = new Contact(79990000001, "andrew@example.com"),
+        };
         static void Main(string[] args)
         {
-            string key;
-            do
+            WriteAllContacts();
+            PhoneBook.Add("Иван", new Contact(79990000003, "ivan@example.com"));
+            WriteAllContacts();
+            if (PhoneBook.TryGetValue("Иван", out Contact contact))
             {
-                Console.WriteLine("Введите текст");
-                string text = Console.ReadLine();
-                var hSet = new HashSet<char>(text);
-                Console.WriteLine("Символов всего " + hSet.Count);
-                char[] symbols = new char[] { '.', ',', '!', '?', ' ' };
-                hSet.ExceptWith(symbols);
-                Console.WriteLine("Символов без знаков препинания " + hSet.Count);
-
-                string numbers = null;
-                for (int i = 0; i < 10; i++)
-                {
-                    numbers += i;
-                }
-                Console.WriteLine("В тексте есть цифры - " + hSet.Overlaps(numbers));
-                Console.WriteLine("Желаете повторить?\nВведите \'Да\', чтобы продолжить");
-                key = Console.ReadLine();
-            } while (key == "Да");
+                contact.PhoneNumber = 123456789;
+            }
+            WriteAllContacts();
         }
+        static void WriteAllContacts() 
+        {
+            Console.WriteLine("Список ваших контактов");
+            foreach (var contact in PhoneBook)
+            {
+                Console.WriteLine(contact.Key + ": " + contact.Value.PhoneNumber);
+            }
+            Console.WriteLine();
+        }
+    }
+    public class Contact 
+    {
+        public Contact(long phoneNumber, String email) 
+        {
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public long PhoneNumber { get; set; }
+        public String Email { get; }
     }
 }
 
