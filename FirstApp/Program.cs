6b9978799d4bb54f9e756dfd339e467b7e9e3028
IvanShmytov <ivanshmytov@mail.ru>
2022-05-30 20:17:38 +0300

    Выполнено задание 14.1.1.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,31 +1,40 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace FirstApp
+namespace LinqTest
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
-            List<string> peopList = new List<string>();
-            foreach (var item in people)
+            // Добавим Россию с её городами
+            var russianCities = new List<City>();
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
+            var shortNames = russianCities.Where(x => x.Name.Length < 11).OrderBy(x => x.Name.Length);
+            foreach (var item in shortNames)
             {
-                bool qwe = item.StartsWith('А');
-                if (qwe)
-                {
-                    peopList.Add(item);
-                }
+                Console.WriteLine(item.Name);
             }
-            peopList.Sort();
-            foreach (var item in peopList)
-            {
-                Console.WriteLine(item);
-            }
-
         }
 
+        // Создадим модель класс для города
+        public class City
+        {
+            public City(string name, long population)
+            {
+                Name = name;
+                Population = population;
+            }
+
+            public string Name { get; set; }
+            public long Population { get; set; }
+        }
     }
 }
 
