f8b43938fac70c4bc3e1c896441a27cfd16dbb97
IvanShmytov <ivanshmytov@mail.ru>
2022-06-01 16:30:28 +0300

    Выполнено задание 14.2.4.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,7 +9,6 @@ namespace LinqTest
 
         static void Main(string[] args)
         {
-            // Наш список студентов
             List<Student> students = new List<Student>
             {
                new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
@@ -17,29 +16,37 @@ namespace LinqTest
                new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
                new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
             };
-            var studApps = from s in students
-                           where s.Age < 27
-                           let yearOfBirth = DateTime.Now.Year - s.Age
-                           select new Application
+            var coarses = new List<Coarse>
+            {
+               new Coarse {Name="Язык программирования C#", StartDate = new DateTime(2020, 12, 20)},
+               new Coarse {Name="Язык SQL и реляционные базы данных", StartDate = new DateTime(2020, 12, 15)},
+            };
+            var CsharpStuds = from s in students   
+                           where s.Age < 29 && s.Languages.Contains("английский")
+                           let birthYear = DateTime.Now.Year - s.Age
+                           from c in coarses
+                           where c.Name == "Язык программирования C#"
+                           select new 
                            {
                                Name = s.Name,
-                               YearOfBirth = yearOfBirth
+                               YearOfBirth = birthYear,
+                               CoarseName = c.Name
                            };
-            foreach (var item in studApps)
+            foreach (var item in CsharpStuds)
             {
-                Console.WriteLine($"Студент \'{item.Name}\' - год рождения {item.YearOfBirth}");
+                Console.WriteLine($"Студент {item.Name} {item.YearOfBirth} г.р. зачислен на курс \'{item.CoarseName}\'");
             }
-        }
-        public class Application
-        {
-            public string Name { get; set; }
-            public int YearOfBirth { get; set; }
         }
         public class Student 
         {
             public string Name { get; set; }
             public int Age { get; set; }
             public List<string> Languages { get; set; }
+        }
+        public class Coarse
+        {
+            public string Name { get; set; }
+            public DateTime StartDate { get; set; }
         }
     }
 }
