f19192ce18271fc817443a75f1ebd3c7adcdbd99
IvanShmytov <ivanshmytov@mail.ru>
2022-06-04 17:36:55 +0300

    Выполнено задание 15.2.3.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,25 +9,14 @@ namespace LinqTest
 
         static void Main(string[] args)
         {
-            var contacts = new List<Contact>()
-            {
-               new Contact() { Name = "Андрей", Phone = 79994500508 },
-               new Contact() { Name = "Сергей", Phone = 799990455 },
-               new Contact() { Name = "Иван", Phone = 79999675334 },
-               new Contact() { Name = "Игорь", Phone = 8884994 },
-               new Contact() { Name = "Анна", Phone = 665565656 },
-               new Contact() { Name = "Василий", Phone = 3434 }
-            };
-            int result = contacts.Count(c => !c.Phone.ToString().StartsWith("7") || c.Phone.ToString().Length != 11);
-            Console.WriteLine("Количество неверных номеров " + result);
+            int[] numbers = {1, 5, 11, 1, 1 };
+            Console.WriteLine(Average(numbers));
         }
-        class Contact
+        static double Average(int[] numbers)
         {
-            public string Name { get; set; }
-            public long Phone { get; set; }
-
+            var result = numbers.Sum();
+            return (double)result / numbers.Length;
         }
-
     }
 }
 
