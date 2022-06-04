82361cde596bbbfb499d20c9eb272c8500562eeb
IvanShmytov <ivanshmytov@mail.ru>
2022-06-04 17:31:11 +0300

    Выполнено задание 15.2.2.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,18 +9,23 @@ namespace LinqTest
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Factorial(7));
-        }
-        static long Factorial(int number)
-        {
-            List<int> numbs = new List<int>();
-            for (int i = 1; i < number + 1; i++)
+            var contacts = new List<Contact>()
             {
-                numbs.Add(i);
-            }
+               new Contact() { Name = "Андрей", Phone = 79994500508 },
+               new Contact() { Name = "Сергей", Phone = 799990455 },
+               new Contact() { Name = "Иван", Phone = 79999675334 },
+               new Contact() { Name = "Игорь", Phone = 8884994 },
+               new Contact() { Name = "Анна", Phone = 665565656 },
+               new Contact() { Name = "Василий", Phone = 3434 }
+            };
+            int result = contacts.Count(c => !c.Phone.ToString().StartsWith("7") || c.Phone.ToString().Length != 11);
+            Console.WriteLine("Количество неверных номеров " + result);
+        }
+        class Contact
+        {
+            public string Name { get; set; }
+            public long Phone { get; set; }
 
-            long result = numbs.Aggregate((x,y) => x * y);
-            return result;
         }
 
     }
