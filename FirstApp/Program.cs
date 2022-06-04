e97cbdf5c79ebb07dc5bdd595c7a29d1068a6676
IvanShmytov <ivanshmytov@mail.ru>
2022-06-04 17:58:29 +0300

    Выполнено задание 15.2.8.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,13 +9,27 @@ namespace LinqTest
 
         static void Main(string[] args)
         {
-            int[] numbers = {1, 5, 11, 1, 1 };
-            Console.WriteLine(Average(numbers));
-        }
-        static double Average(int[] numbers)
-        {
-            var result = numbers.Sum();
-            return (double)result / numbers.Length;
+            List<int> Numbs = new List<int>();
+            while (true)
+            {
+                Console.WriteLine("Введите число для добавления");
+                var input = Console.ReadLine();
+                var parsed = Int32.TryParse(input.ToString(), out int numb);
+                if (!parsed)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Вы ввели не число");
+                }
+                else
+                {
+                    Numbs.Add(numb);
+                    Console.WriteLine("Количество чисeл в списке " + Numbs.Count());
+                    Console.WriteLine("Сумма чисел в списке " + Numbs.Sum());
+                    Console.WriteLine("Наибольшее число в списке " + Numbs.Max());
+                    Console.WriteLine("Наименьшее число в списке " + Numbs.Min());
+                    Console.WriteLine("Среднее арифметическое чисел в списке " + Numbs.Average());
+                }
+            }
         }
     }
 }
