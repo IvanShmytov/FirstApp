c1f5a6bb6a89df7cac5aef9e9048bced359d154d
IvanShmytov <ivanshmytov@mail.ru>
2022-06-04 16:54:22 +0300

    Выполнено задание 15.2.1.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,46 +9,20 @@ namespace LinqTest
 
         static void Main(string[] args)
         {
+            Console.WriteLine(Factorial(7));
+        }
+        static long Factorial(int number)
+        {
+            List<int> numbs = new List<int>();
+            for (int i = 1; i < number + 1; i++)
+            {
+                numbs.Add(i);
+            }
 
-            ShowCommonLetters();
-            //ShowUnicSymbols();
+            long result = numbs.Aggregate((x,y) => x * y);
+            return result;
         }
-        static void ShowCommonLetters() 
-        {
-            Console.WriteLine("Введите 1 слово");
-            string word1 = Console.ReadLine();
-            Console.WriteLine("Введите 2 слово");
-            string word2 = Console.ReadLine();
-            if (string.IsNullOrEmpty(word1) || string.IsNullOrEmpty(word2))
-            {
-                Console.WriteLine("Вы ввели пустой текст");
-                return;
-            }
-            var comLetters = word1.Intersect(word2).ToArray();
-            Console.WriteLine("Общие буквы");
-            foreach (var item in comLetters)
-            {
-                Console.WriteLine(item);
-            }
-        }
-        static void ShowUnicSymbols() 
-        {
-            Console.WriteLine("Введите текст");
-            string text = Console.ReadLine();
-            if (string.IsNullOrEmpty(text))
-            {
-                Console.WriteLine("Вы ввели пустой текст");
-                return;
-            }
-            string punct = ".,!?;: ";
-            var result = text.Except(punct).ToArray();
-            Console.WriteLine("Уникальные символы в тексте");
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
-        }
-        
+
     }
 }
 
